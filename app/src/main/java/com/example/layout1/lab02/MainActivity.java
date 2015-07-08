efadf18a3aabe4d25cfa0c6c39935ae41fa230ac
Nathan Santos <nathanemail.ns@gmail.com>
2015-07-08 15:06:08 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/layout1/lab02/MainActivity.java
+++ app/src/main/java/com/example/layout1/lab02/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+* @author Nathan Santos
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
